2a7b845b92894704af23c767476cfc4f4830f66a
MeNoSmartBrain <ya.hemmer@gmail.com>
2021-05-07 01:25:05 +0200

    Customer: Reverted StringBuilder 2

--- src/Customer.java
+++ src/Customer.java
@@ -30,12 +30,13 @@ class Customer {
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1)
-                frequentRenterPoints ++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(thisAmount) + "\n";
             totalAmount += thisAmount;
         }
         //add footer lines
