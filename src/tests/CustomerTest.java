7fe51e7e2a080d9f4dec918e280c6718af19fc84
MeNoSmartBrain <ya.hemmer@gmail.com>
2021-05-07 01:20:47 +0200

    Customer: Added Tests

---
+++ src/tests/CustomerTest.java
@@ -0,0 +1,18 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+public class CustomerTest {
+
+    @org.junit.Test
+    public void addRental() {
+    }
+
+    @org.junit.Test
+    public void getName() {
+    }
+
+    @org.junit.Test
+    public void statement() {
+    }
+}
