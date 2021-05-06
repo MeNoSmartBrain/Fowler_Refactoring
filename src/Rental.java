58e2b2af5e6f7929371216db869ec2b006579bb3
MeNoSmartBrain <ya.hemmer@gmail.com>
2021-05-07 00:35:45 +0200

    Added java doc comments for methods, constructors and class attributes for class Rental

--- src/Rental.java
+++ src/Rental.java
@@ -1,13 +1,35 @@
 class Rental {
+    /**
+     * movie : Movie object
+     */
     private Movie movie;
+    /**
+     * daysRented : Time past of rental represented by an Integer
+     */
     private int daysRented;
+
+    /**
+     * Constructor for the Rental class with the two ...
+     * @param newmovie : Movie of rental
+     * @param newdaysRented : Time since the rental began
+     */
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
+    /**
+     * Getter-Function
+     * @return daysRented : Get the days since the rental began
+     */
     public int getDaysRented() {
         return daysRented;
     }
+
+    /**
+     * Getter-Function
+     * @return movie : Get the movie that was rented
+     */
     public Movie getMovie() {
         return movie;
     }
