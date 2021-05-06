326d6c0ced976d6b6fa7d095a7cb5237b48be2d1
MeNoSmartBrain <ya.hemmer@gmail.com>
2021-05-07 01:05:00 +0200

    Customer: Fixed preceding whitespaces when '++'

--- src/Customer.java
+++ src/Customer.java
@@ -26,10 +26,10 @@ class Customer {
             //determine amounts for each line
             thisAmount = amountFor(each);
             // add frequent renter points
-            frequentRenterPoints ++;
+            frequentRenterPoints++;
             // add bonus for a two day new release rental
             if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
-                frequentRenterPoints ++;
+                frequentRenterPoints++;
             //show figures for this rental
             result.append("\t").append(each.getMovie().getTitle()).append("\t").append("\t").append(each.getDaysRented()).append("\t").append(String.valueOf(thisAmount)).append("\n");
             totalAmount += thisAmount;
