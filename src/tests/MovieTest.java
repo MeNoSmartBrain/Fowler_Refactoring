cffe42c7a507ac5673bf94644a24547685b2fd69
MeNoSmartBrain <ya.hemmer@gmail.com>
2021-05-07 01:32:58 +0200

    Added Test for classes

---
+++ src/tests/MovieTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovieTest {
+
+    @Test
+    public void getPriceCode() {
+    }
+
+    @Test
+    public void setPriceCode() {
+    }
+
+    @Test
+    public void getTitle() {
+    }
+}
---
+++ src/tests/RentalTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RentalTest {
+
+    @Test
+    public void getDaysRented() {
+    }
+
+    @Test
+    public void getMovie() {
+    }
+}
